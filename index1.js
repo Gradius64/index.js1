99c7de899d73619ea6ed8983d4274d6fdb31c5c1
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-19 15:23:31 -0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ['How much is the vehicle cost?'];
+['What is the size of the vehicle?'];
+
+['What year is the vehicle?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
